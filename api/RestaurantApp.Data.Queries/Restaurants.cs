20aedd4f2e1c0f97601e5504a39836a11ac3cfa6
Didier CAUVIN <didier.cauvin@gmail.com>
2017-04-25 16:30:16 +0200

    merge

--- api/RestaurantApp.Data.Queries/Restaurants.cs
+++ api/RestaurantApp.Data.Queries/Restaurants.cs
@@ -8,6 +8,10 @@ namespace RestaurantApp.Data.Queries
 
         public const string GET_BY_ID = "MATCH(r:RESTAURANT {uuid: {id}}) -[l: LOCATED_IN]->(s: STREET) -[:STREET_OF]->(c: CITY) RETURN r.uuid as Id, r.name as RestaurantName, r.Description as RestaurantDescription, l.number as StreetNumber, s.name as Street, c.zipcode as ZipCode, c.name as CityName";
 
-        public const string CREATE = "MERGE (r:RESTAURANT {uuid: {uuid}, name: {name}, description: {description}})-[l:LOCATED_IN {number: {streetNumber}}]->(s:STREET {name: {street}})-[:STREET_OF]->(c:CITY {name: {city}, zipcode: {zipcode}})";
+        public const string CREATE = @"MERGE (r:RESTAURANT {uuid: {uuid}, name: {name}})
+                                       MERGE(s:STREET { name: {street}})
+                                       MERGE(c:CITY { name: {city}, zipcode: {zipcode}})
+                                       MERGE(s)-[:STREET_OF]->(c)
+                                       MERGE(r)-[:LOCATED_IN {number: {streetNumber}}]->(s)";
     }
 }
